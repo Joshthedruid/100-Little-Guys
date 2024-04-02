29d5598d2776dc52f7597b9be4ebe914fae616e2
Joshthedruid <33952609+Joshthedruid@users.noreply.github.com>
2024-04-01 22:26:52 -0500

    expanding guycan function
    Started work on GuyCan.  Now has an array of guys, a function to add them, and a function to pop and spawn them.  Needs testing to confirm it all works still.

--- GuyCan.gd
+++ GuyCan.gd
@@ -6,9 +6,31 @@ extends Area2D
 #TODO: let the GuyCan reabsorb guys when killed
 #TODO: Create datastructure for remembering which guys are in and not
 
+#gets game area to add guys to
+var gameArea = get_parent()
+
+#makes an array to store guys
+var guyArray = []
+
+#loads a specific guy and adds it to the array
+func addGuy(myString):
+	var path = "res://Unique_Guys/" + myString
+	var myGuy = load(path)
+	guyArray.append(myGuy)
+
+#adds a guy to the scene and removes it from the array
+func spawnGuy():
+	if(guyArray.size() > 0):
+		guyArray.shuffle()
+		var newGuy = guyArray.pop_back()
+		gameArea.add_child(newGuy.instantiate())
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#cloud of all 100 guys to add to array.  probably a better way to do this?
+	addGuy("flash_guy.tscn")
+	addGuy("leapfrog_guy.tscn")
+	addGuy("leftbeard_guy.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
